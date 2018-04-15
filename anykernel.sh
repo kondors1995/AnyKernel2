8b01a7174be4b9b719d4acbc547a0163647d571a
NATO66613 <normandija1945@gmail.com>
2018-04-15 23:19:57 +0000

    This is revolt

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=Oreo EAS by NATO66613 @ xda-developers
+kernel.string=Revolt by NATO66613 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
